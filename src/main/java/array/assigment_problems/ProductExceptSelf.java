ae6f867f2de3684d77c178848d73370ab3803852
FunctionFuturist <aakritib404@gmail.com>
2026-09-20 22:33:18 +0530

    Create ProductExceptSelf.java

---
+++ src/main/java/array/assigment_problems/ProductExceptSelf.java
@@ -0,0 +1,34 @@
+package array.assigment_problems;
+
+import java.util.Arrays;
+
+public class ProductExceptSelf {
+
+    public static int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] answer = new int[n];
+
+        // Pass 1: Accumulate left prefix products
+        answer[0] = 1;
+        for (int i = 1; i < n; i++) {
+            answer[i] = answer[i - 1] * nums[i - 1];
+        }
+
+        // Pass 2: Accumulate right suffix products in-place
+        int rightProduct = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            answer[i] = answer[i] * rightProduct;
+            rightProduct *= nums[i];
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = {1, 2, 3, 4};
+        System.out.println("Output 1: " + Arrays.toString(productExceptSelf(nums1)));
+
+        int[] nums2 = {-1, 1, 0, -3, 3};
+        System.out.println("Output 2: " + Arrays.toString(productExceptSelf(nums2)));
+    }
+}
